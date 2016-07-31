193495e3f3fc0e565f422e778acdf3bdea386b76
Jeff Collier <jeffscollier@gmail.com>
2016-07-31 00:17:11 -0400

    basic capability

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,23 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) {        
+  
+  im <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+
+  setInverted <- function(inverted_matrix) im <<- inverted_matrix
+
+  getInverted <- function() im
+
+    list(set = set, get = get,
+     setInverted = setInverted,
+     getInverted = getInverted)
 
 }
 
@@ -11,5 +27,15 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  ## Return a matrix that is the inverse of 'x'
+  im <- x$getInverted()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data, ...)
+  x$setInverted(im)
+  iim
 }
